9f7d99b4dc2a061cc3cc2d12f4843194c33f948e
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2017-11-30 20:44:03 +0800

    added script for dataset generation

--- data/deal_images.py
+++ data/deal_images.py
@@ -1,55 +1,60 @@
-depth_file = open("./depths.csv", "r")
-t = depth_file.readline()
-d1 = [float(d) for d in t.split(",")]
-
-img_file = open("./images.csv", "r")
-t2 = img_file.readline()
-d2 = [int(d) for d in t2.split(",")]
-
-depth_file.close();
-img_file.close();
-
-import cv2
-
 import numpy as np
-
-depth_img = np.array(d1)
-img_img = np.array(d2, dtype=np.uint8)
-
-
-mx = np.max(depth_img)
-mi = np.min(depth_img)
-
-print mx, mi
-
-depth_img = (depth_img - mi) / (mx - mi)
-depth_img = np.reshape(depth_img, [640, 480])
-depth_img = np.transpose(depth_img)
-depth_img = np.reshape(depth_img, [640 * 480])
-
-img = np.reshape(img_img, [3, 640, 480])
-img = np.transpose(img, axes=[2, 1, 0])
-img = np.reshape(img, [480*640, 3])
-img = img * (1.0 / 256)
-
-
 import random
 
-k = random.uniform(0.7, 1.0)
-A = np.array([k,k,k])
-beta = random.uniform(-1.5, -0.5)
+def generate_dataset(depth_data, img_data):
+    depth_img = np.array(depth_data)
+    img_img = np.array(img_data, dtype=np.uint8)
 
-tx = np.reshape(np.exp(depth_img * beta), (480*640, 1))
+    depth_img = np.reshape(depth_img, [640, 480])
+    depth_img = np.transpose(depth_img)
+    depth_img = np.reshape(depth_img, [640 * 480])
 
-print A
+    img = np.reshape(img_img, [3, 640, 480])
+    img = np.transpose(img, axes=[2, 1, 0])
+    img = np.reshape(img, [480*640, 3])
+    img = img * (1.0 / 256)
 
-img_out = tx * img + A * (1-tx)
-# img_out = A * (1-tx)
+    k = random.uniform(0.6, 1.0)
+    A = np.array([k,k,k])
+    beta = random.uniform(-1.5, -0.5)
 
-img_out = np.reshape(img_out, (480, 640, 3))
+    tx = np.reshape(np.exp(depth_img * beta), (480*640, 1))
 
-cv2.imshow("imgout", img_out)
+    img_out = tx * img + A * (1-tx)
 
+    orig_img = np.reshape(img, (480*640*3))
+    img_out = np.reshape(img_out, (480*640*3))
 
-while True:
-    cv2.waitKey(0)
+    img_out_show = np.reshape(img_out, (480, 640, 3))
+
+    return [",".join([str(k) for k in img_out.tolist()]), ",".join([str(k) for k in orig_img.tolist()])]
+
+depth_file = open("./depths.csv", "r")
+img_file = open("./images.csv", "r")
+hazed_img_file = open("./hazed_images.csv", "w")
+original_img_file = open("./original_images.csv", "w")
+
+TOT_DATA = 2248
+REPEAT_TIMES = 5;
+
+tot = 0
+for i in range(TOT_DATA):
+    
+    t = depth_file.readline()
+    d1 = [float(d) for d in t.split(",")]
+    t2 = img_file.readline()
+    d2 = [int(d) for d in t2.split(",")]
+
+    for j in range(REPEAT_TIMES):
+        l1, l2 = generate_dataset(d1, d2)
+
+        hazed_img_file.write(str(tot) + "," + l1 + "\n")
+        original_img_file.write(str(tot) + "," + l2 + "\n")
+
+        tot += 1
+        print(tot)
+    
+depth_file.close()
+img_file.close()
+hazed_img_file.close()
+original_img_file.close()
