c1a9406351bde014bc26cc3905a846631dc11241
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-01-07 23:48:59 +0800

    Add G and D

--- CGAN/cgan.py
+++ CGAN/cgan.py
@@ -1,4 +1,15 @@
 import tensorflow as tf
+import tensorlayer as tl
+import math
+import numpy as np
+
+if "concat_v2" in dir(tf):
+    def concat(tensors, axis, *args, **kwargs):
+        return tf.concat_v2(tensors, axis, *args, **kwargs)
+else:
+    def concat(tensors, axis, *args, **kwargs):
+        return tf.concat(tensors, axis, *args, **kwargs)
+
 
 class CGAN(object):
     def __init__(self, sess, batch_size, z_dim, learning_rate):
@@ -13,9 +24,34 @@ class CGAN(object):
 
         self.data_X, self.data_Y = load_data()
         self.num_batches = len(self.data_X) // self.batch_size
-        
 
-    def discriminator(self, x, y, is_training=True, reuse=False):
+    def Conv_BN_PReLu(self, network, input_channel, output_channel, scope, reuse, is_training, kernel_size=3):
+        with tf.variable_scope(scope, reuse=reuse):
+            network = tl.layers.Conv2dLayer(network,
+                                shape=[kernel_size, kernel_size, input_channel, output_channel],
+                                strides=[1, 1, 1, 1],
+                                padding='SAME',
+                                W_init=tf.truncated_normal_initializer(stddev=0.02),
+                                b_init=tf.constant_initializer(value=0.0),
+                                name=scope+'/conv')
+            network = tl.layers.BatchNormLayer(network, is_train=is_training, name=scope+'/bn')
+            network = tl.layers.PReluLayer(network, a_init=0.2, channel_shared=True, name=scope+'/pReLu')
+            return network
+
+    def DeConv_BN_ReLu(self, network, input_channel, output_channel, scope, reuse, is_training, kernel_size=3):
+        with tf.variable_scope(scope, reuse=reuse):
+            network = tl.layers.DeConv2dLayer(network,
+                                              shape=[kernel_size, kernel_size, input_channel, output_channel],
+                                              output_shape=[-1, network.outputs._shape[1], network.outputs._shape[2], output_channel],
+                                              strides=[1, 1, 1, 1],
+                                              padding='SAME',
+                                              W_init=tf.truncated_normal_initializer(stddev=0.02),
+                                              b_init=tf.constant_initializer(value=0.0),
+                                              name=scope + '/deconv')
+            network = tl.layers.BatchNormLayer(network, act=tf.nn.relu, is_train=is_training, name=scope+'/bn')
+            return network
+
+    def discriminator(self, x, y, ndf, is_training=True, reuse=False):
         with tf.variable_scope("discriminator", reuse=reuse):
 
             # merge image and label
@@ -33,13 +69,34 @@ class CGAN(object):
 
             return out, out_logit, net
             '''
+            y = concat([x, y], 3)
+            Input = tl.layers.InputLayer(y, "DInput")
+            CB_K = tl.layers.Conv2dLayer(Input,
+                                        shape=[3, 3, 3, ndf],
+                                        strides=[1, 1, 1, 1],
+                                        padding='SAME',
+                                        W_init=tf.truncated_normal_initializer(stddev=0.02),
+                                        b_init=tf.constant_initializer(value=0.0),
+                                        name='CB_K')
+            CB_K = tl.layers.BatchNormLayer(CB_K, is_train=is_training, name="CB_K_BN")
+            CBP_2K = self.Conv_BN_PReLu(CB_K, ndf, 2 * ndf, "CBP_2K", reuse, is_training)
+            CBP_4K = self.Conv_BN_PReLu(CBP_2K, 2 * ndf, 4 * ndf, "CBP_4K", reuse, is_training)
+            CBP_8K = self.Conv_BN_PReLu(CBP_4K, 4 * ndf, 8 * ndf, "CBP_8K", reuse, is_training)
+            Conv_1 = tl.layers.Conv2dLayer(CBP_8K,
+                                        shape=[3, 3, 8 * ndf, 1],
+                                        strides=[1, 1, 1, 1],
+                                        padding='SAME',
+                                        W_init=tf.truncated_normal_initializer(stddev=0.02),
+                                        b_init=tf.constant_initializer(value=0.0),
+                                        name='Conv_1')
+            out = tf.nn.sigmoid(Conv_1.outputs)
+            return out
 
-    def generator(self, z, y, is_training=True, reuse=False):
+    def generator(self, z, x, ngf, is_training=True, reuse=False):
         with tf.variable_scope("generator", reuse=reuse):
-
             # merge noise and label
             '''
-            z = concat([z, y], 1)
+            z = concat([z, x], 1)
 
             net = tf.nn.relu(bn(linear(z, 1024, scope='g_fc1'), is_training=is_training, scope='g_bn1'))
             net = tf.nn.relu(bn(linear(net, 128 * 7 * 7, scope='g_fc2'), is_training=is_training, scope='g_bn2'))
@@ -52,6 +109,26 @@ class CGAN(object):
 
             return out
         '''
+            #z = concat([z, x], 1)
+
+            Input = tl.layers.InputLayer(x, "GInput")
+            G_CBP_K1 = self.Conv_BN_PReLu(Input, 3, ngf, "G_CBP(K)_1", reuse, is_training)
+            G_CBP_K2 = self.Conv_BN_PReLu(G_CBP_K1, ngf, ngf, "G_CBP(K)_2", reuse, is_training)
+            G_CBP_K3 = self.Conv_BN_PReLu(G_CBP_K2, ngf, ngf, "G_CBP(K)_3", reuse, is_training)
+            G_CBP_K4 = self.Conv_BN_PReLu(G_CBP_K3, ngf, ngf, "G_CBP(K)_4", reuse, is_training)
+            G_CBP_K_div_2 = self.Conv_BN_PReLu(G_CBP_K4, ngf, ngf / 2, "G_CBP(K/2)", reuse, is_training)
+            G_CBP_1 = self.Conv_BN_PReLu(G_CBP_K_div_2, ngf / 2, 1, "G_CBP(1)", reuse, is_training)
+            G_DBR_K_div_2 = self.DeConv_BN_ReLu(G_CBP_1, 1, ngf / 2, "G_DBR(K/2)", reuse, is_training)
+            G_DBR_K1 = self.DeConv_BN_ReLu(G_DBR_K_div_2, ngf / 2, ngf, "G_DBR(K)_1", reuse, is_training)
+            G_DBR_K1.outputs = G_DBR_K1.outputs + G_CBP_K4.outputs
+            G_DBR_K2 = self.DeConv_BN_ReLu(G_DBR_K1, ngf, ngf, "G_DBR(K)_2", reuse, is_training)
+            G_DBR_K3 = self.DeConv_BN_ReLu(G_DBR_K2, ngf, ngf, "G_DBR(K)_3", reuse, is_training)
+            G_DBR_K3.outputs = G_DBR_K3.outputs + G_CBP_K2.outputs
+            G_DBR_K4 = self.DeConv_BN_ReLu(G_DBR_K3, ngf, ngf, "G_DBR(K)_4", reuse, is_training)
+            G_DBR_3 = self.DeConv_BN_ReLu(G_DBR_K4, ngf, 3, "G_DBR(3)", reuse, is_training)
+            G_DBR_3.outputs = G_DBR_3.outputs + z
+            out = tf.nn.tanh(G_DBR_3.outputs, name="out")
+            return out
 
     def build_model(self):
         # x : hazed images, labels
