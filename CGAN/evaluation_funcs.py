94d654add169a3b55d35a4da7b0a59a52bb0ff06
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-01-19 22:22:10 -0500

    Add test

--- CGAN/evaluation_funcs.py
+++ CGAN/evaluation_funcs.py
@@ -27,8 +27,10 @@ def test_image(origin, recovered, batch_size):
     SSIMs = []
     UQIs = []
     for i in range(batch_size):
-        origin_img = origin[i]
-        recovered_img = recovered[i]
+        origin_img = (origin[i] + 1) / 2
+        recovered_img = (recovered[i] + 1) / 2
+        recovered_img = recovered_img.astype(np.float64)
+        print(origin_img.dtype, recovered_img.dtype)
         PSNRs.append(PSNR(origin_img, recovered_img))
         SSIMs.append(SSIM(origin_img, recovered_img))
         UQIs.append(UQI(origin_img, recovered_img))
